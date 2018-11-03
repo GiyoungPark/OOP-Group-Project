7ee0f5ee0549aee34c88fe96e62443f5d250f086
Chase Meyer <chase.e.meyer@gmail.com>
2018-11-02 20:25:22 -0500

    removal of licence commits

--- Banking System/src/BankSystem/OpeningScreen.java
+++ Banking System/src/BankSystem/OpeningScreen.java
@@ -1,12 +1,5 @@
-/*
- * OpeningScreen.java
 
- * Copyright (C) 2018 Chase Meyer <chase.e.meyer@gmail.com>
- *
- * Distributed under terms of the GNU license.
- */
-
-import Accounts.*
+import Accounts.*;
 
 package BankSystem;
 
