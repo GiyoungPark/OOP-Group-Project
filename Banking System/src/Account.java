cc743579775c7a0ac62be10bba15ef3b6aa5de5a
YoungPark1004 <44853310+YoungPark1004@users.noreply.github.com>
2018-11-07 18:15:20 -0600

    Update Account.java

--- Banking System/src/Account.java
+++ Banking System/src/Account.java
@@ -1,3 +1,5 @@
+// Account - For verification gives 3 attempts Account # and PIN # (SSN ??)
+// We need 4 functions - View Balance, Deposit, Withdraw, Transfer
 
 static class Account {
     // ass sssssss
