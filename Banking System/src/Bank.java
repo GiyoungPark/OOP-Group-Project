214413320333978698377110e7d52f21a97ccf3a
Chase Meyer <chase.e.meyer@gmail.com>
2018-11-07 19:03:26 -0600

    Bank change

--- Banking System/src/Bank.java
+++ Banking System/src/Bank.java
@@ -3,6 +3,7 @@ public class Bank
 {
     int size;
     Account head;
+    Account tail;
 
 	public Bank() {
 
@@ -11,17 +12,31 @@ public class Bank
     void removeAccount(int accountNumber){
         Account tmpAccnt = searchAccounts(accountNumber);
 
-        // if the account is at the tail of the list assign it to null
+        // remove from head
         if (tmpAccnt == this.head){
             head = tmpAccnt.getNext();
+            head.setPrev(null);
+            tmpAccnt = null;
+        } else {
+        // remove form body
+            Account tmpAccnt2 = tmpAccnt.getPrev();
+            // check that temp is not Account before the tail Account
+            if (tmpAccnt2 == this.tail) {
+                if (this.size != 1)
+                    this.tail = tmpAccnt2.getPrev();
+                tmpAccnt2 = null;
+            }
+            // remove Account
+            tmpAccnt2.setNext(tmpAccnt.getNext().getNext());
+            if (tmpAccnt2.getNext() == null) {
+                this.tail = tmpAccnt2;
+            } else {
+                tmpAccnt2 = tmpAccnt2.getNext();
+                tmpAccnt2.setPrev(tmpAccnt2.getPrev().getPrev());
+            }
             tmpAccnt = null;
         }
-        else if (tmpAccnt.getNext() == null) {
-            tmpAccnt = null;
-        }else {
-            Account tmpAccnt2 = tmpAccnt;
-            tmpAccnt.setNext(tmpAccnt.getNext().getNext());
-
-
+        this.size--;
+    }
 }
 
