4c291f6c84695a0b12db60d723fb0ed159df24f2
Dogi258 <43687397+Dogi258@users.noreply.github.com>
2018-11-07 19:43:24 -0600

    Update Bank.java

--- Banking System/src/Bank.java
+++ Banking System/src/Bank.java
@@ -8,7 +8,43 @@ public class Bank
 	public Bank() {
 
 	}
-
+	//////////////////////Mehtods//////////////////////
+	 void addAccount(int accountNumber, int index) {
+    	//Input validation
+    	if (index > size || index < 0) {
+    		System.out.println("Invalid index");
+    		return;
+        }
+    	//Initialize new account
+    	Account newAccount = new Account();
+    	
+    	//Insert at the head of the list
+    	if (index == 0) {
+            newAccount.setNext(head);
+            if (size != 0)
+                head.setPrev(newAccount);
+            head = newAccount;
+            if (size == 0)
+                tail = newAccount;
+        }
+    	//Insert anywhere else
+    	else {
+            Account temp = head;
+            for(int i=0; i < index - 1; i++, temp = temp.getNext());
+            newAccount.setNext(temp.getNext());
+            temp.setNext(newAccount);
+            newAccount.setPrev(temp);
+            if(newAccount.getNext() == NULL)
+                tail = newAccount;
+            else {
+                temp = newAccount.getNext();
+                temp.setPrev(newAccount);
+            } 
+        }
+    	//Increment size
+    	size++;
+	 }
+	
     void removeAccount(int accountNumber){
         Account tmpAccnt = searchAccounts(accountNumber);
 
