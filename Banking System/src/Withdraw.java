6c1631bbf37dd8062e5557ec80a4cbb9e2d6fa0a
YoungPark1004 <44853310+YoungPark1004@users.noreply.github.com>
2018-11-09 18:06:36 -0600

    Update Withdraw.java

--- Banking System/src/Withdraw.java
+++ Banking System/src/Withdraw.java
@@ -1,20 +1,28 @@
 
-
+import java.util.Scanner;
 //<<< header file?
 
 
-public class Withdraw extends Account{
+public Withdraw extends Account{
         boolean withdraw;
         double amount;
         
-        if (amount < 0.0) {
-            throw new IllegalArgumentException("Cannot withdraw a negative amount");
-        }
-        if (amount > balance) {
-            throw new IllegalStateException("Cannot overdraw an account");
-        }
+        Scanner scan = new Scanner(System.in);
+        System.out.println("withdraw amount : ");
+        amount = scan.nextDouble();
         
-        balance = (balance - amount);
+        
+        
+            if (amount <= 0.0) {
+            throw new IllegalArgumentException("Cannot withdraw a negative amount or 0 ");
+        
+            if (amount > balance) {
+                throw new IllegalStateException("Cannot overdraw an account");
+            }
+
+            balance = (balance - amount);
 }
         
+              
+}
               
