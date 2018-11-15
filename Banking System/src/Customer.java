f75a10a5edb194a4120d43aeb2d3552e35c58c4b
Dogi258 <43687397+Dogi258@users.noreply.github.com>
2018-11-14 19:13:02 -0600

    Update Customer.java

--- Banking System/src/Customer.java
+++ Banking System/src/Customer.java
@@ -3,7 +3,12 @@ public class Customer extends Bank{
         int menu = 0;
 	
 	void startMenu(Account acc){
-		while (true){
+		
+		acc.validatePIN();
+		
+		if (validate = true){
+			
+			while (true){
 			System.out.println("(1) View Balance");
 			System.out.println("(2) View Last Deposited Amount");
 			System.out.println("(3) Deposit");
@@ -14,18 +19,31 @@ public class Customer extends Bank{
 			menu = sc.nextInt(); 
 			
 			if (menu == 1){
-				
+				acc.Query();
 			}
 			elseif (menu == 2) {
-				
+				acc.lastDepositAmount();
 			}
+			elseif (menu == 3) {
+				acc.deposit();
+			}
+			elseif (menu == 4) {
+				acc.withdraw();
+			}
+			elseif (menu == 5) {
+				acc.tranfer();
+			}
+			elseif (menu == 6) {
+				break;
+			}
+			else{
+				System.out.println("Invalid Input");
+			}
+			
 			
 		}
 		
 	}
-	
-	
-	
 	
 }
 
