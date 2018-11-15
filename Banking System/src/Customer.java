af60b0e7aee909a0baa2fab974a2b51f8e6ebc06
Dogi258 <43687397+Dogi258@users.noreply.github.com>
2018-11-14 18:51:04 -0600

    Update Customer.java

--- Banking System/src/Customer.java
+++ Banking System/src/Customer.java
@@ -1,6 +1,31 @@
 public class Customer extends Bank{
-	public Customer() {
-
+	Scanner sc = new Scanner(System.in);
+        int menu = 0;
+	
+	void startMenu(Account acc){
+		while (true){
+			System.out.println("(1) View Balance");
+			System.out.println("(2) View Last Deposited Amount");
+			System.out.println("(3) Deposit");
+			System.out.println("(4) Withdraw");
+			System.out.println("(5) Tranfer Funds to Another Account");
+			System.out.println("(6) Cancel");
+			
+			menu = sc.nextInt(); 
+			
+			if (menu == 1){
+				
+			}
+			elseif (menu == 2) {
+				
+			}
+			
+		}
+		
 	}
+	
+	
+	
+	
 }
 
