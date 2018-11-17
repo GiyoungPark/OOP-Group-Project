5901b4b379ebab7b83154f3dd064f8552edf6dd2
Dogi258 <43687397+Dogi258@users.noreply.github.com>
2018-11-16 19:00:18 -0600

    Update main.java

--- Banking System/src/main.java
+++ Banking System/src/main.java
@@ -1,67 +1,33 @@
-//Content from http://www.tutorialspoint.com/swing/swing_jbutton.htm
+import java.util.Scanner;
 
-
-import java.awt.*;
-import java.awt.event.*;
-import javax.swing.*;
- 
-class SwingControlDemo{
-   private JFrame mainFrame;
-   private JLabel headerLabel;
-   private JLabel statusLabel;
-   private JPanel controlPanel;
-
-   public SwingControlDemo(){
-      prepareGUI();
-   }
+  
    public static void main(String[] args){
-      SwingControlDemo  swingControlDemo = new SwingControlDemo();     		// I think this needs a while loop: while(1) 
-      swingControlDemo.showButtonDemo();
+	   Bank bank = new Bank();
+	   Customer customer = new Customer();
+	   
+	   Scanner sc = new Scanner(System.in);
+	   int menu = 0;
+	   
+	   while (true){
+		   System.out.println("(1) Customer");
+		   System.out.println("(2) Bank Staff");
+		   System.out.println("(3) Add Interest");
+		   
+		   menu = sc.nextInt();
+		   
+		   if (menu == 1){
+			   System.out.println("Account Number?");
+			   int acc = sc.nextInt();
+			   customer.startMenu(bank.searchAccounts(acc));
+			}
+		   if (menu == 2){
+			   //bank staff
+			}
+		   if (menu == 3){
+			   //Add interest
+			}
+		   
+		   
+	   }
    }
-   private void prepareGUI(){
-      mainFrame = new JFrame("Main Console");
-      mainFrame.setSize(400,400);
-      mainFrame.setLayout(new GridLayout(3, 1));
-      
-      mainFrame.addWindowListener(new WindowAdapter() {
-         public void windowClosing(WindowEvent windowEvent){
-            System.exit(0);
-         }        
-      });    
-      headerLabel = new JLabel("", JLabel.CENTER);        
-      statusLabel = new JLabel("",JLabel.CENTER);    
-      statusLabel.setSize(350,100);
-
-      controlPanel = new JPanel();
-      controlPanel.setLayout(new FlowLayout());
-
-      mainFrame.add(headerLabel);
-      mainFrame.add(controlPanel);
-      mainFrame.add(statusLabel);
-      mainFrame.setVisible(true);  
-   }
- 
-   private void showButtonDemo(){
-      headerLabel.setText("Which best describes you?"); 
-
-      JButton BankStaffButton = new JButton("Bank Staff");
-      JButton CustomerButton = new JButton("Customer");
-
-      BankStaffButton.addActionListener(new ActionListener() {
-         public void actionPerformed(ActionEvent e) {
-		//BankStaff.BankStaff(); //<- I think this is how you call the bank staff file
-            statusLabel.setText("Entering Bank Staff Console");		
-         }
-      });
-      CustomerButton.addActionListener(new ActionListener() {
-         public void actionPerformed(ActionEvent e) {
-		// Customer.StartMenu(acc);
-            statusLabel.setText("Entering Customer Console");			
-	 }
-      });
-      controlPanel.add(BankStaffButton);
-      controlPanel.add(CustomerButton);       
-
-      mainFrame.setVisible(true);  
-   }
-}
+  
