c7bd349537252814cd4cd9bb2cf2a918373646f4
Chase Meyer <chase.e.meyer@gmail.com>
2018-11-26 20:07:18 -0600

    Update Bank.java

--- Banking System/src/Bank.java
+++ Banking System/src/Bank.java
@@ -13,82 +13,99 @@ public class Bank
 	}
 	//////////////////////Mehtods//////////////////////
     //boolean type determins type of accont 0 is checking, 1 is saving
-	 void addAccount(int accountNumber, int index, boolean type) {
-    	//Input validation
-    	if (index > size || index < 0) {
-    		System.out.println("Invalid index");
-    		return;
-        }
+	 void addAccount(int accountNumber,  boolean type) {
     	//Initialize new account
-        Account newAllAccount = new Account();
         if (type){
             Saving newAccount = new Saving();
+            //Insert at the head of the list
+            newAccount.setNext(shead);
+            if (size != 0)
+                shead.setPrev(newAccount);
+            shead = newAccount;
+            if (size == 0)
+                stail = newAccount;
+
         }else {
             Checking newAccount = new Checking();
+            //Insert at the head of the list
+            newAccount.setNext(chead);
+            if (size != 0)
+                chead.setPrev(newAccount);
+            chead = newAccount;
+            if (size == 0)
+                ctail = newAccount;
+
         }
 
-        newAllAccount.setNext(head);
+        Account newAllAccount = new Account();
+    	//Insert at the head of the list
         if (size != 0)
             head.setPrev(newAllAccount);
-        else if (size == 0)
+        head = newAllAccount;
+        if (size == 0)
             tail = newAllAccount;
 
-    	//Insert at the head of the list
-    	if (index == 0) {
-            if (type)
-                newAccount.setNext(shead);
-            else
-                newAccount.setNext(chead);
-            if (size != 0)
-                if (type)
-                    shead.setPrev(newAccount);
-                else
-                    chead.setPrev(newAccount);
-            head = newAllAccount;
-            if (type)
-                shead = newAccount;
-            else
-                chead = newAccount;
-            if (size == 0)
-                if (type)
-                    stail = newAccount;
-                else
-                    ctail = newAccount;
-        }
-
-    	//Insert anywhere else
-    	else {
-            if (type)
-                Saving temp = shead;
-            else
-                Checking temp = chead;
-            for(int i=0; i < index - 1; i++, temp = temp.getNext());
-            newAccount.setNext(temp.getNext());
-            temp.setNext(newAccount);
-            newAccount.setPrev(temp);
-            if(newAccount.getNext() == null)
-                if (type)
-                    stail = newAccount;
-                else
-                    ctail = newAccount;
-            else {
-                temp = newAccount.getNext();
-                temp.setPrev(newAccount);
-            }
-        }
     	//Increment size
-    	size++;
+    	this.size++;
 	 }
 
     void removeAccount(int accountNumber){
+        Account tmpAccnt = searchAccounts(accountNumber);
         if (tmpAccnt.checking){
-            Checking tmpAccnt = searchCheckings(accountNumber);
-            Checking head = this.chead;
-            Checking tail = this.ctail;
+            Checking tmpAccntC = searchCheckings(accountNumber);
+            // remove from head
+            if (tmpAccntC == chead){
+                chead = tmpAccntC.getNext();
+                chead.setPrev(null);
+                tmpAccntC = null;
+            } else {
+            // remove form body
+                Checking tmpAccnt2 = searchCheckings(accountNumber);
+
+                // check that temp is not Account before the tail Account
+                if (tmpAccnt2 == tail) {
+                    if (this.size != 1)
+                        tail = tmpAccnt2.getPrev();
+                    tmpAccnt2 = null;
+                }
+                // remove Account
+                tmpAccnt2.setNext(tmpAccnt.getNext().getNext());		//should this be tmpAccnt2.setNext(tmpAccnt2.getNext().getNext()); ?
+                if (tmpAccnt2.getNext() == null) {
+                    tail = tmpAccnt2;
+                } else {
+                    tmpAccnt2 = tmpAccnt2.getNext();
+                    tmpAccnt2.setPrev(tmpAccnt2.getPrev().getPrev());
+                }
+                tmpAccnt = null;
+            }
+            this.size--;
         } else if (tmpAccnt.saving){
-            Saving tmpAccnt = searchSavings(accountNumber);
-            Saving head = this.shead;
-            Saving tail = this.stail;
+            Saving tmpAccntS = searchSavings(accountNumber);
+            // remove from head
+            if (tmpAccntS == shead){
+                shead = tmpAccntS.getNext();
+                shead.setPrev(null);
+                tmpAccntS = null;
+            } else {
+            // remove form body
+                Saving tmpAccnt2 = searchSavings(accountNumber);
+                // check that temp is not Account before the tail Account
+                if (tmpAccnt2 == tail) {
+                    if (this.size != 1)
+                        tail = tmpAccnt2.getPrev();
+                    tmpAccnt2 = null;
+                }
+                // remove Account
+                tmpAccnt2.setNext(tmpAccnt.getNext().getNext());		//should this be tmpAccnt2.setNext(tmpAccnt2.getNext().getNext()); ?
+                if (tmpAccnt2.getNext() == null) {
+                    tail = tmpAccnt2;
+                } else {
+                    tmpAccnt2 = tmpAccnt2.getNext();
+                    tmpAccnt2.setPrev(tmpAccnt2.getPrev().getPrev());
+                }
+                tmpAccnt = null;
+            }
+            this.size--;
         }
 
         // remove from head
@@ -98,16 +115,7 @@ public class Bank
             tmpAccnt = null;
         } else {
         // remove form body
-            if (tmpAccnt.checking){
-                Checking tmpAccnt2 = new Checking();
-                Checking tmpAccnt2 = searchCheckings(accountNumber);
-            }else if (tmpAccnt.saving){
-                Saving tmpAccnt2 = new Saving();
-                Saving tmpAccnt2 = searchSavings(accountNumber);
-            }else{
-                System.out.println("ERROR");
-                return;
-            }
+            Account tmpAccnt2 = new Account();
 
             // check that temp is not Account before the tail Account
             if (tmpAccnt2 == tail) {
@@ -126,38 +134,31 @@ public class Bank
             tmpAccnt = null;
         }
         this.size--;
+
     }
-    Saving searchSavings(int accountNumber, boolean type) {
-        if (type){
-            Saving tmpAccnt = this.head;
-        } else {
-            Checking tmpAccnt = this.head;
-    	while (tmpAccnt != null && tmpAccnt.getAccountNumber() != accountNumber) {
-        	tmpAccnt = tmpAccnt.getNext();
-    	}
+    Saving searchSavings(int accountNumber) {
+        Saving tmpAccnt = this.shead;
+        while (tmpAccnt != null && tmpAccnt.getAccountNumber() != accountNumber) {
+            tmpAccnt = tmpAccnt.getNext();
+        }
         if (tmpAccnt == null)
                 System.out.println("Account Number not found: " + accountNumber);
 
-    	return tmpAccnt;
+        return tmpAccnt;
     }
-    Checking searchCheckings(int accountNumber, boolean type) {
-        if (type){
-            Saving tmpAccnt = this.head;
-        } else {
-            Checking tmpAccnt = this.head;
-    	while (tmpAccnt != null && tmpAccnt.getAccountNumber() != accountNumber) {
-        	tmpAccnt = tmpAccnt.getNext();
-    	}
+
+    Checking searchCheckings(int accountNumber) {
+        Checking tmpAccnt = this.chead;
+        while (tmpAccnt != null && tmpAccnt.getAccountNumber() != accountNumber) {
+            tmpAccnt = tmpAccnt.getNext();
+        }
         if (tmpAccnt == null)
                 System.out.println("Account Number not found: " + accountNumber);
-
-    	return tmpAccnt;
+        return tmpAccnt;
     }
-    Account searchAccounts(int accountNumber, boolean type) {
-        if (type){
-            Saving tmpAccnt = this.head;
-        } else {
-            Checking tmpAccnt = this.head;
+
+    Account searchAccounts(int accountNumber) {
+        Account tmpAccnt = new Account();
     	while (tmpAccnt != null && tmpAccnt.getAccountNumber() != accountNumber) {
         	tmpAccnt = tmpAccnt.getNext();
     	}
