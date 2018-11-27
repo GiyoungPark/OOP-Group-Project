4f5c21ca8bd0a436cb6415d39df8fe6f8cc695a0
Chase Meyer <chase.e.meyer@gmail.com>
2018-11-26 20:33:40 -0600

    Update Checking.java

--- Banking System/src/Checking.java
+++ Banking System/src/Checking.java
@@ -5,5 +5,15 @@ public class Checking extends Account {
     Checking() {
         super.checking = true;
     }
-
+    void setNext(Checking next){
+	    this.next = next;
+    }
+    Checking getNext(){
+	    return this.next;
+    }
+    void setPrev(Checking prev){
+	    this.prev = prev;
+    }
+    Checking getPrev(){
+	    return this.prev;
 }
