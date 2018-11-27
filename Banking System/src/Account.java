d6b269e986c785adc88b378a9dffab04a6de01da
GiyoungPark <40605909+GiyoungPark@users.noreply.github.com>
2018-11-26 18:46:42 -0600

    Update Account.java

--- Banking System/src/Account.java
+++ Banking System/src/Account.java
@@ -13,9 +13,7 @@ public class Account {
     double lastDeposit = 0;
     Account next;
     Account prev;
-    /* do we give variables here? (gave me build errors without those)
-    double amount;
-    boolean validatePIN; */
+   
 
     Account(){
         this.accountNumber = 0;
